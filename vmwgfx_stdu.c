ee5e28cf11f668501402d50da2e4ef01f9806e20
Sinclair Yeh <syeh@vmware.com>
2016-12-30 14:20:55 -0800

    vmwgfx: Add and connect connector helper function
    Since the link between connector and encoder is always fixed in our case,
    just return the one encoder.
    
    These helpers won't be called until we flip on the atomic support
    flag or set drm_crtc_funcs->set_config to using the atomic
    helper.
    
    Signed-off-by: Sinclair Yeh <syeh@vmware.com>
    Reviewed-by: Thomas Hellstrom <thellstrom@vmware.com>

--- vmwgfx_stdu.c
+++ vmwgfx_stdu.c
@@ -1191,6 +1191,12 @@ static const struct drm_connector_funcs vmw_stdu_connector_funcs = {
 };
 
 
+static const struct
+drm_connector_helper_funcs vmw_stdu_connector_helper_funcs = {
+	.best_encoder = vmw_du_connector_best_encoder,
+};
+
+
 
 /******************************************************************************
  * Screen Target Display Plane Functions
@@ -1531,6 +1537,8 @@ static int vmw_stdu_init(struct vmw_private *dev_priv, unsigned unit)
 		DRM_ERROR("Failed to initialize connector\n");
 		goto err_free;
 	}
+
+	drm_connector_helper_add(connector, &vmw_stdu_connector_helper_funcs);
 	connector->status = vmw_du_connector_detect(connector, false);
 	vmw_connector_state_to_vcs(connector->state)->is_implicit = false;
 
