cdc4c0291cfe094ebdd1ced781babf4b3335ec45
Thomas Hellstrom <thellstrom@vmware.com>
2018-03-02 14:07:31 +0100

    vmwgfx: Avoid pinning fbdev framebuffers
    fbdev framebuffers were previously pinned to be able to keep them mapped
    across updates.
    
    This commit introduces a mechanism that instead revalidates the map on
    each update, keeping the map cached across updates. The cached map is torn
    down if the underlying pages change. Typically on buffer object moves and
    swapouts.
    
    This should be nicer to the system when we have resource contention.
    
    Testing done: Basic fbdev functionality under Fedora 27.
    
    Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Deepak Rawat <drawat@vmware.com>
    Reviewed-by: Sinclair Yeh <syeh@vmware.com>

--- vmwgfx_buffer.c
+++ vmwgfx_buffer.c
@@ -862,6 +862,7 @@ static void vmw_move_notify(struct ttm_buffer_object *bo,
  */
 static void vmw_swap_notify(struct ttm_buffer_object *bo)
 {
+	vmw_resource_swap_notify(bo);
 	ttm_bo_wait(bo, false, false);
 }
 
